a4b761044c3aed1b1daf580b06ff290fde4a4c51
tuguu4 <tuguldur.solution@gmail.com>
2023-06-28 13:04:07 +0800

    key name changed

--- my_instasnce.tf
+++ my_instasnce.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "intro" {
     ami = "ami-076015d9e4b6b6a0b"
     instance_type = "t2.micro"
     availability_zone = "ap-southeast-1a"
-    key_name = "dove-key"
+    key_name = "ansible-sing-key"
     vpc_security_group_ids = ["sg-0fe8c435c7dcc8729"]
     tags = {
         Name = "Dove-Instance"

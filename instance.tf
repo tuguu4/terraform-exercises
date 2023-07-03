639133205ef5af12cf50fc8f3760ce285f9340f2
MTG - SH - ASA - Tuguldur Bolormaa <tuguldur_bo@golomtbank.com>
2023-07-03 12:44:10 +0800

    Instance creation file updated

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "dove_inst" {
-    ami = var.AMI[var.REGION]
+    ami = var.AMIS[var.REGION]
     instance_type = "t2.micro"
     availability_zone = var.ZONE1
     key_name = "ansible-sing-key"
